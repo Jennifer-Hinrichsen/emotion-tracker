9fea4415b79cd8543e60e30e55a22acce859ca3b
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-10-02 10:25:35 +0200

    create and add EmotionList-component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import { initialEntries } from "@/lib/entries";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Emotion Tracker</h1>
+      <ul>
+        {initialEntries.map((entry) => (
+          <li key={entry.id}>
+            <p>Type: {entry.emotion}</p>
+            <p>Intensity: {entry.intensity}</p>
+            <p>Notes: {entry.notes}</p>
+            <p>Date and Time: {entry.dateTime}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
