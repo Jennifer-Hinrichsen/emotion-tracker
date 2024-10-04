56e73b7a28e77a8eeeb9bfe9ec4322c5b169825c
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-10-04 11:24:56 +0200

    add router back functionality

--- components/EmotionDetails.js
+++ components/EmotionDetails.js
@@ -1,13 +1,19 @@
 import { initialEntries } from "@/lib/entries";
 
 console.log(initialEntries);
-export default function EmotionDetails({ emotion }) {
+export default function EmotionDetails({ emotion, onBack }) {
   return (
-    <div>
-      <h2>Type: {emotion.emotion}</h2>
-      <p>Intensity: {emotion.intensity}</p>
-      <p>Date and Time: {emotion.dateTime}</p>
-      <p>Notes: {emotion.notes}</p>
-    </div>
+    <>
+      <button type="button" aria-label="navigate-back" onClick={onBack}>
+        ←
+      </button>
+      <h1>Emotion Details</h1>
+      <div>
+        <h2>Type: {emotion.emotion}</h2>
+        <p>Intensity: {emotion.intensity}</p>
+        <p>Date and Time: {emotion.dateTime}</p>
+        <p>Notes: {emotion.notes}</p>
+      </div>
+    </>
   );
 }
