a9dbdf4545e84ee017b40f88d881894be019742a
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-10-07 11:56:43 +0200

    add emotion inputs to the select emotion dropdown

--- components/EmotionForm.js
+++ components/EmotionForm.js
@@ -1,15 +1,32 @@
 import { emotions } from "@/lib/emotions";
+import { useState } from "react";
 
 export default function EmotionForm() {
+  const [selectedEmotion, setSelectedEmotion] = useState();
+
+  function handleEmotionChange(event) {
+    setSelectedEmotion(event.target.value);
+  }
+
   return (
     <>
       <h2>Add new Emotion</h2>
       <form>
-        <label htmlFor="emotion">Emotion (Type)</label>
-        <select id="emotion" name="emotion">
+        <label htmlFor="emotion">Emotion (Type)*</label>
+        <select
+          value={selectedEmotion}
+          onChange={handleEmotionChange}
+          id="emotion"
+          name="emotion"
+          required
+        >
           <option value="">---Choose an emotion---</option>
-          {emotions.map((emotion) => {
-            return <option key={emotion} value={emotion}></option>;
+          {emotions.map((emotion, index) => {
+            return (
+              <option key={index} value={emotion}>
+                {emotion}
+              </option>
+            );
           })}
         </select>
       </form>
