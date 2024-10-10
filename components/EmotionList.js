fc29893029f7c2e24589844e0c9b409d7ca38ea0
Stephanie Tack <stephierathjen@gmx.de>
2024-10-10 13:45:40 +0200

    implement check for emotions and show appropriate message

--- components/EmotionList.js
+++ components/EmotionList.js
@@ -5,13 +5,17 @@ import EmotionCard from "./EmotionCard";
 export default function EmotionList({ entries }) {
   return (
     <StyledEmotionList>
-      {entries.map((entry) => (
-        <StyledLink key={entry.id} href={`emotion/${entry.id}`}>
-          <li>
-            <EmotionCard entry={entry} />
-          </li>
-        </StyledLink>
-      ))}
+      {entries.length === 0 ? ( // Überprüfe, ob keine Einträge vorhanden sind
+        <p>At the moment there are not emotions. Please add an emotion.</p>
+      ) : (
+        entries.map((entry) => (
+          <StyledLink key={entry.id} href={`emotion/${entry.id}`}>
+            <li>
+              <EmotionCard entry={entry} />
+            </li>
+          </StyledLink>
+        ))
+      )}
     </StyledEmotionList>
   );
 }
