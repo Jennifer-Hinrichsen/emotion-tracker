3dc55dc53e71f0521314e4ea1bdf37b113a62a4c
Stephanie Tack <stephierathjen@gmx.de>
2024-10-18 12:04:09 +0200

    Add filter functionality in EmotionList

--- components/EmotionList.js
+++ components/EmotionList.js
@@ -1,17 +1,42 @@
 import styled from "styled-components";
 import Link from "next/link";
 import EmotionCard from "./EmotionCard";
+import { useState } from "react";
+import { emotions } from "@/lib/emotions";
 
 export default function EmotionList({ objects }) {
+  const [selectedFilter, setSelectedFilter] = useState("");
+
+  // Verwende filteredData, um die Objekte basierend auf dem Filter zu filtern
+  const filteredData = selectedFilter
+    ? objects.filter((item) => item.emotion === selectedFilter)
+    : objects; // Wenn nichts ausgewählt ist, zeige alle
+
   return (
     <StyledEmotionList>
-      {objects.length === 0 ? (
+      <StyledDiv>
+        <h2>Filter emotion type</h2>
+        {emotions.map((emotion) => (
+          <StyledSpan
+            key={emotion}
+            // Wenn emotion bereits ausgewählt ist, setze den Filter zurück
+            onClick={() =>
+              setSelectedFilter(emotion === selectedFilter ? "" : emotion)
+            }
+            isSelected={emotion === selectedFilter}
+          >
+            {emotion}
+          </StyledSpan>
+        ))}
+      </StyledDiv>
+
+      {filteredData.length === 0 ? (
         <StyledMessage>
           At the moment there are no emotions in the list. Please add an
           emotion.
         </StyledMessage>
       ) : (
-        objects.map((object) => (
+        filteredData.map((object) => (
           <StyledLink key={object.id} href={`emotion/${object.id}`}>
             <li>
               <EmotionCard object={object} />
@@ -27,6 +52,11 @@ const StyledEmotionList = styled.ul`
   padding: 0;
 `;
 
+const StyledDiv = styled.div`
+  display: flex;
+  background-color: grey;
+`;
+
 const StyledMessage = styled.p`
   text-align: center;
   color: #777;
@@ -39,5 +69,20 @@ const StyledLink = styled(Link)`
   color: #000;
   &:visited {
     color: inherit;
+  }
+`;
+
+const StyledSpan = styled.span`
+  margin: 0 10px;
+  padding: 5px 10px;
+  border-radius: 15px;
+  cursor: pointer;
+  text-decoration: ${({ isSelected }) => (isSelected ? "underline" : "none")};
+  background-color: ${({ isSelected }) =>
+    isSelected
+      ? "#ddd"
+      : "#f0f0f0"}; // Ändere die Farbe des ausgewählten Filters
+  &:hover {
+    background-color: #e0e0e0;
   }
 `;
