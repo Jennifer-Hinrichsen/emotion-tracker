635c83288fcdc7bafcd2b6b032fef3111e3bacf4
Stephanie Tack <stephierathjen@gmx.de>
2024-11-14 10:49:00 +0100

    add site createemotiontype.js

---
+++ pages/createemotiontype.js
@@ -0,0 +1,6 @@
+import EmotionForm from "@/components/EmotionForm";
+import Heading from "@/components/Heading";
+
+export default function CreateEmotionType() {
+  return <Heading>Create Emotion Type</Heading>;
+}
