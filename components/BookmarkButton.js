3ac61c89b4b697d6b26efd27bfe27c164be76a62
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-11-19 11:42:16 +0100

    add darkmode for bookmark svg

--- components/BookmarkButton.js
+++ components/BookmarkButton.js
@@ -4,7 +4,11 @@ import BookmarkIconFilled from "assets/bookmarkicons/Bookmark-filled.svg";
 
 export default function BookmarkButton({ isBookmarked, onToggleBookmark }) {
   return (
-    <StyledButton onClick={onToggleBookmark} aria-label="Toggle Bookmark">
+    <StyledButton
+      onClick={onToggleBookmark}
+      aria-label="Toggle Bookmark"
+      isBookmarked={isBookmarked}
+    >
       {isBookmarked ? <BookmarkIconFilled /> : <BookmarkIcon />}
     </StyledButton>
   );
@@ -20,4 +24,10 @@ const StyledButton = styled.button`
   border: none;
   cursor: pointer;
   touch-action: manipulation;
+  color: ${(props) =>
+    props.isBookmarked ? "var(--color-cards-foreground)" : "inherit"};
+  body.dark-theme & {
+    color: ${(props) =>
+      props.isBookmarked ? "var(--color-secondary)" : "inherit"};
+  }
 `;
