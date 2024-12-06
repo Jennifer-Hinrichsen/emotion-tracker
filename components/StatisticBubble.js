a78fe6b20b15bde71d8cb201e68f37172d77c27b
Stephanie Tack <stephierathjen@gmx.de>
2024-12-06 12:27:06 +0100

    develop logic for the data with return of the text

--- components/StatisticBubble.js
+++ components/StatisticBubble.js
@@ -1,14 +1,40 @@
-export default function StatisticBubble({ emotionTypes }) {
-  console.log(emotionTypes);
+import categorizeIntensity from "@/lib/categorizeIntensity";
+
+export default function StatisticBubble({ emotions, emotionTypes }) {
+  const typeStatisticData = emotions.reduce((acc, emotion) => {
+    const id = emotion.type._id;
+    if (!acc[id]) {
+      acc[id] = { totalIntensity: 0, count: 0 };
+    }
+    acc[id].totalIntensity += emotion.intensity;
+    acc[id].count += 1;
+    return acc;
+  }, {});
+
+  const filteredEmotionTypes = emotionTypes
+    .filter((emotionType) => typeStatisticData[emotionType._id] !== undefined)
+    .map((emotionType) => {
+      const typeData = typeStatisticData[emotionType._id];
+      const average = Math.round(typeData.totalIntensity / typeData.count);
+
+      emotionType.count = typeData.count;
+      emotionType.average = categorizeIntensity(average);
+      console.log(emotionType);
+      return emotionType;
+    })
+    .toSorted(
+      (emotionTypeA, emotionTypeB) => emotionTypeB.count - emotionTypeA.count
+    );
 
   return (
     <>
       <ul>
-        {emotionTypes.map((emotion) => (
-          <li key={emotion._id}>
+        {filteredEmotionTypes.map((emotionType) => (
+          <li key={emotionType._id}>
             <p>
-              You felt <strong>{emotion.type.name}</strong> xx times, with an
-              average intensity classified as xxx.
+              You felt <strong>{emotionType.name} </strong>
+              {emotionType.count} times, with an average intensity of{" "}
+              {emotionType.average}.
             </p>
           </li>
         ))}
--- pages/statistic/index.js
+++ pages/statistic/index.js
@@ -3,12 +3,18 @@ import StatisticBubble from "@/components/StatisticBubble";
 import useSWR from "swr";
 
 export default function StatisticPage() {
-  const { data: emotionTypes } = useSWR("/api/emotionTypes");
+  const { data: emotions, isLoading } = useSWR("/api/emotionEntries");
+  const { data: emotionTypes, isLoading: isLoadingTypes } =
+    useSWR("/api/emotionTypes");
+
+  if (isLoading || isLoadingTypes) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <>
       <Heading>My Statistic</Heading>
-      <StatisticBubble emotionTypes={emotionTypes} />
+      <StatisticBubble emotions={emotions} emotionTypes={emotionTypes} />
     </>
   );
 }
