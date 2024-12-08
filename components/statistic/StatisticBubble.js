1acfd8691fd845e0affd7149789ea918acc85c49
Stephanie Tack <stephierathjen@gmx.de>
2024-12-08 20:37:23 +0100

    set up first styling

--- components/statistic/StatisticBubble.js
+++ components/statistic/StatisticBubble.js
@@ -1,6 +1,7 @@
 import categorizeIntensity from "@/lib/categorizeIntensity";
 import styled from "styled-components";
 import StatisticCircles from "./StatisticCircles";
+import StatisticFilter from "./StatisticFilter";
 
 export default function StatisticBubble({ emotions, emotionTypes }) {
   const typeStatisticData = emotions.reduce((acc, emotion) => {
@@ -46,27 +47,31 @@ export default function StatisticBubble({ emotions, emotionTypes }) {
     )
   );
 
-  return filteredEmotionTypes.length === 0 ? (
-    <StyledMessage $isNoEntry>
-      No entries found to create your emotion statistic.
-    </StyledMessage>
-  ) : (
-    <StyledList>
-      {filteredEmotionTypes.map((emotionType) => (
-        <StyledListItem key={emotionType._id}>
-          <StatisticCircles
-            count={emotionType.count}
-            color={emotionType.color}
-            maxCount={maxCount}
-          />
-          <StyledMessage>
-            You felt <strong>{emotionType.name} </strong>
-            {emotionType.count} times, with an average intensity of{" "}
-            {emotionType.average}.
-          </StyledMessage>
-        </StyledListItem>
-      ))}
-    </StyledList>
+  return (
+    <>
+      <StatisticFilter />
+      {filteredEmotionTypes.length === 0 ? (
+        <StyledMessage $isNoEntry>
+          No entries found to create your emotion statistic.
+        </StyledMessage>
+      ) : (
+        <StyledList>
+          {filteredEmotionTypes.map((emotionType) => (
+            <StyledListItem key={emotionType._id}>
+              <StatisticCircles
+                count={emotionType.count}
+                color={emotionType.color}
+                maxCount={maxCount}
+              />
+              <StyledMessage>
+                You felt <strong>{emotionType.name}</strong> {emotionType.count}{" "}
+                times, with an average intensity of {emotionType.average}.
+              </StyledMessage>
+            </StyledListItem>
+          ))}
+        </StyledList>
+      )}
+    </>
   );
 }
 
--- components/statistic/StatisticFilter.js
+++ components/statistic/StatisticFilter.js
@@ -1,1 +1,70 @@
-export default function StatisticFilter() {}
+import styled from "styled-components";
+
+export default function StatisticFilter() {
+  return (
+    <Styled>
+      {/* <StyledP>Filter period of time</StyledP> */}
+      <StyledZwo>
+        <StyledDrei>
+          <label htmlFor="month">Month</label>
+          <StyledDateAndTimeInput
+            type="month"
+            id="month"
+            name="month"
+          ></StyledDateAndTimeInput>
+        </StyledDrei>
+        <StyledDrei>
+          <label htmlFor="calendarWeek">Calendarweek</label>
+          <StyledDateAndTimeInput
+            type="datetime"
+            id="calendarWeek"
+            name="calendarWeek"
+            // type="text"
+            // id="calendarWeek"
+            // name="calendarWeek"
+            // pattern="\d{4}-W\d{2}"
+            // placeholder="YYYY-WW"
+            // title="Format: YYYY-WW (e.g., 2024-W12)"
+          ></StyledDateAndTimeInput>
+        </StyledDrei>
+      </StyledZwo>
+    </Styled>
+  );
+}
+
+const Styled = styled.div`
+  background-color: var(--color-slider-intensity);
+  padding: 12px;
+`;
+
+// const StyledP = styled.div`
+//   padding: 5px 0 20px 0;
+//   font-weight: bold;
+//   font-size: 1.2em;
+// `;
+
+const StyledZwo = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const StyledDrei = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+const StyledDateAndTimeInput = styled.input`
+  padding: 0 0 6px 0;
+  width: 100%;
+  border: none;
+  border-bottom: 1px dotted var(--color-form-foreground);
+  background-color: transparent;
+  color: var(--color-form-foreground);
+  font-size: 1rem;
+
+  &::-webkit-calendar-picker-indicator {
+    cursor: pointer;
+    color: var(--color-form-foreground);
+  }
+`;
