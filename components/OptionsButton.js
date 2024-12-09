9e2e9a070224585da940d0a7a8056ec4aa110c62
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-12-09 11:59:15 +0100

    add styled popup menu with close functionality

--- components/OptionsButton.js
+++ components/OptionsButton.js
@@ -1,11 +1,29 @@
 import styled from "styled-components";
 import OptionsIcon from "assets/optionsIcon/dots-circle-horizontal.svg";
+import { useState } from "react";
 
 export default function OptionsButton() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
+  const handleClosePopup = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
-    <StyledButton>
-      <OptionsIcon />
-    </StyledButton>
+    <>
+      <StyledButton onClick={handleClick}>
+        <OptionsIcon />
+      </StyledButton>
+      {isMenuOpen && (
+        <StyledPopupMenu>
+          <StyledCloseButton onClick={handleClosePopup}>×</StyledCloseButton>
+        </StyledPopupMenu>
+      )}
+    </>
   );
 }
 
@@ -23,4 +41,29 @@ const StyledButton = styled.button`
   body.dark-theme & {
     color: var(--color-secondary);
   }
+`;
+
+const StyledPopupMenu = styled.div`
+  position: absolute;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  bottom: 8px;
+  right: 16px;
+  width: 30%;
+  height: 50%;
+  border-radius: 8px;
+  background-color: var(--color-secondary);
+`;
+
+const StyledCloseButton = styled.button`
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+  background: none;
+  border: none;
+  font-size: 1.5rem;
+  color: var(--color-background-cards);
+  cursor: pointer;
 `;
