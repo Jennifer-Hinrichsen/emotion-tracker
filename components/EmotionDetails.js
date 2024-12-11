338f2ea45890bce9174533065d24eb6a5f7b0530
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-12-11 09:40:11 +0100

    refactor share functionality within created component

--- components/EmotionDetails.js
+++ components/EmotionDetails.js
@@ -3,6 +3,7 @@ import Heading from "./Heading";
 import styled from "styled-components";
 import Link from "next/link";
 import { useState } from "react";
+import ShareButton from "./ShareButton";
 
 export default function EmotionDetails({
   emotion,
@@ -19,27 +20,6 @@ export default function EmotionDetails({
   function handleDelete() {
     onDeleteEmotion(emotion._id);
     toggleDeleteDialog();
-  }
-
-  async function shareEmotion() {
-    const shareData = {
-      title: "Emotion Card",
-      text: `I'm feeling ${emotion.type.name} with intensity ${
-        emotion.intensity
-      }. Note: ${emotion.notes || ""}`,
-      url: window.location.href,
-    };
-
-    try {
-      if (navigator.share) {
-        await navigator.share(shareData);
-      } else {
-        alert("Sharing is not supported in this browser.");
-      }
-    } catch (error) {
-      console.error("Error sharing:", error);
-      alert("Failed to share. Please try again.");
-    }
   }
 
   return (
@@ -63,9 +43,7 @@ export default function EmotionDetails({
       <StyledButtonDelete type="button" onClick={toggleDeleteDialog}>
         Delete
       </StyledButtonDelete>
-      <StyledButtonShare type="button" onClick={shareEmotion}>
-        Share
-      </StyledButtonShare>
+      <ShareButton emotion={emotion} />
 
       {isDialogOpen && (
         <StyledDialogOverlay>
@@ -166,19 +144,5 @@ const StyledButtonCancel = styled.button`
   cursor: pointer;
   &:hover {
     background-color: #7f8c8d;
-  }
-`;
-const StyledButtonShare = styled.button`
-  float: right;
-  margin-right: 20px;
-  margin: 10px;
-  padding: 10px 20px;
-  background-color: #2ecc71;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  &:hover {
-    background-color: #27ae60;
   }
 `;
