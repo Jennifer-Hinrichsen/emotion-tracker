409225b559a8703e17ed8f0fb8549c4274dfee12
Stephanie Tack <stephierathjen@gmx.de>
2024-12-12 21:27:32 +0100

    update the API configuration

--- pages/api/images/ImageUpload.js
+++ pages/api/images/ImageUpload.js
@@ -1,23 +1,42 @@
 import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+(async function () {
+  // Configuration
+  cloudinary.config({
+    cloud_name: "dd2rnrfjz",
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 
-export default async function handler(request, response) {
-  if (request.method === "POST") {
-    const { file } = request.body; // erwarte die Bilddatei oder URL
-    try {
-      const uploadResponse = await cloudinary.uploader.upload(file, {
-        folder: "emotion-tracker", // Optional: Ordnerstruktur
-      });
-      response.status(200).json({ url: uploadResponse.secure_url });
-    } catch (error) {
-      response.status(500).json({ error: "Upload failed", details: error });
-    }
-  } else {
-    response.status(405).json({ error: "Method not allowed" });
-  }
-}
+  // Upload an image
+  const uploadResult = await cloudinary.uploader
+    .upload(
+      "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+      {
+        public_id: "shoes",
+      }
+    )
+    .catch((error) => {
+      console.log(error);
+    });
+
+  console.log(uploadResult);
+
+  // Optimize delivery by resizing and applying auto-format and auto-quality
+  const optimizeUrl = cloudinary.url("shoes", {
+    fetch_format: "auto",
+    quality: "auto",
+  });
+
+  console.log(optimizeUrl);
+
+  // Transform the image: auto-crop to square aspect_ratio
+  const autoCropUrl = cloudinary.url("shoes", {
+    crop: "auto",
+    gravity: "auto",
+    width: 500,
+    height: 500,
+  });
+
+  console.log(autoCropUrl);
+})();
