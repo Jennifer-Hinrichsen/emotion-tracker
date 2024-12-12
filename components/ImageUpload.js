eb306ebbb01e5b84785d0fed5d029ce10204bb9d
Stephanie Tack <stephierathjen@gmx.de>
2024-12-12 21:29:10 +0100

    install cloudinary react SDK, and create a component ImageUpload

--- components/ImageUpload.js
+++ components/ImageUpload.js
@@ -1,3 +1,17 @@
+import React from "react";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { auto } from "@cloudinary/url-gen/actions/resize";
+import { autoGravity } from "@cloudinary/url-gen/qualifiers/gravity";
+import { AdvancedImage } from "@cloudinary/react";
+
 export default function ImageUpload() {
-  return <p>Image Upload</p>;
+  const cld = new Cloudinary({ cloud: { cloudName: "dd2rnrfjz" } });
+
+  // Use this sample image or upload your own via the Media Explorer
+  const img = cld
+    .image("e9llkelep2tyo3eayqhv")
+    .format("auto") // Optimize delivery by resizing and applying auto-format and auto-quality
+    .quality("auto")
+    .resize(auto().gravity(autoGravity()).width(100).height(100)); // Transform the image: auto-crop to square aspect_ratio
+  return <AdvancedImage cldImg={img} />;
 }
