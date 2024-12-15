389ad0b42892bb48251b9f331198f359501700bb
Stephanie Tack <stephierathjen@gmx.de>
2024-12-15 10:09:07 +0100

    add imageUrl field to model, improve API error handling, and add new method

--- pages/api/emotionEntries/[id].js
+++ pages/api/emotionEntries/[id].js
@@ -5,7 +5,7 @@ export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
 
-  if (request.method === "GET")
+  if (request.method === "GET") {
     try {
       const emotionEntry = await EmotionEntry.findById(id).populate("type");
 
@@ -18,17 +18,50 @@ export default async function handler(request, response) {
       console.error("Error fetching emotion entry:", error);
       response.status(500).json({ status: "Server Error" });
     }
+  }
 
   if (request.method === "PUT") {
     const emotionData = request.body;
-    await EmotionEntry.findByIdAndUpdate(id, emotionData);
-    return response.status(200).json({ status: `Emotion ${id} updated!` });
+    try {
+      await EmotionEntry.findByIdAndUpdate(id, emotionData);
+      return response.status(200).json({ status: `Emotion ${id} updated!` });
+    } catch (error) {
+      console.error("Error updating emotion entry:", error);
+      response.status(500).json({ status: "Server Error" });
+    }
+  }
+
+  if (request.method === "PATCH") {
+    const { imageUrl } = request.body;
+
+    try {
+      // Nur das imageUrl aktualisieren
+      const updatedEmotionEntry = await EmotionEntry.findByIdAndUpdate(
+        id,
+        { imageUrl }, // Nur das imageUrl-Feld wird geändert
+        { new: true } // Gibt das aktualisierte Dokument zurück
+      );
+
+      if (!updatedEmotionEntry) {
+        return response.status(404).json({ status: "Emotion Entry Not Found" });
+      }
+
+      response.status(200).json(updatedEmotionEntry);
+    } catch (error) {
+      console.error("Error updating image URL:", error);
+      response.status(500).json({ status: "Server Error" });
+    }
   }
 
   if (request.method === "DELETE") {
-    await EmotionEntry.findByIdAndDelete(id);
-    response
-      .status(200)
-      .json({ status: `Emotion ${id} successfully deleted.` });
+    try {
+      await EmotionEntry.findByIdAndDelete(id);
+      response
+        .status(200)
+        .json({ status: `Emotion ${id} successfully deleted.` });
+    } catch (error) {
+      console.error("Error deleting emotion entry:", error);
+      response.status(500).json({ status: "Server Error" });
+    }
   }
 }
